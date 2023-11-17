d30718a6d013483bf0796fd488edb66ba90dc6d9
moonjeong4 <jungmoon0528@gmail.com>
2023-11-17 07:56:33 -0500

    layout for products

---
+++ src/app/products/layout.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styles from "./layout.module.css";
+
+export default function ProductsLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div>
+      <div className={styles.div}>
+        <nav className={styles.nav}>
+          <a href="">여성옷</a>
+          <a href="">남성옷</a>
+        </nav>
+      </div>
+      {children}
+    </div>
+  );
+}
